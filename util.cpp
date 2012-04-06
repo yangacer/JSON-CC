9944799506d91129bda760ffb49cc46abab6fc4b
yang acer <yangacer@gmail.com>
2012-04-06 23:34:53 +0800

    Separate def from header.

---
+++ util.cpp
@@ -0,0 +1,66 @@
+#include "util.hpp"
+#include <boost/spirit/include/support_ostream_iterator.hpp>
+#include <boost/variant/apply_visitor.hpp>
+
+namespace yangacer{
+namespace json{
+  
+template <typename OIter>
+escaped_string<OIter>::escaped_string()
+: escaped_string::base_type(esc_str)
+{
+  esc_char.add('\b', "\\b")('\f', "\\f")('\n', "\\n")
+    ('\r', "\\r")('\t', "\\t")('\\', "\\\\")
+    ('\'', "\\\'")('\"', "\\\"")('/',"\\/")
+    ;
+  esc_str =   karma::lit("\"")
+    << *(esc_char | karma::byte_)
+    <<  karma::lit("\"")
+    ;
+}
+
+template escaped_string<boost::spirit::ostream_iterator>::escaped_string();
+
+escaped_string<boost::spirit::ostream_iterator> print::str_gen;
+
+void print::operator()(bool const b) const
+{
+  b?(*os_)<<"true":(*os_)<<"false";
+}
+
+void print::operator()(std::string const& s) const
+{ 
+  escaped_string<boost::spirit::ostream_iterator> str_gen;
+  boost::spirit::ostream_iterator sink(*os_);
+  karma::generate(sink, str_gen, s);
+}
+
+void print::operator()(array_t const &v) const
+{
+  (*os_)<<"[";
+  for(size_t i=0;i<v.size();++i){
+    boost::apply_visitor(*this, v[i]);
+    if(i + 1 != v.size())
+      (*os_)<<",";
+  }
+  (*os_)<<"]";
+}
+
+void print::operator()(object_t const &m) const
+{
+  using namespace std;
+  (*os_)<<"{";
+  object_t::const_iterator i = m.begin();
+  object_t::const_iterator j = m.begin();
+  while(i != m.end()){
+    (*this)(i->first);
+    (*os_)<<" : ";
+    boost::apply_visitor(*this, i->second);
+    if(++i != m.end())
+      (*os_)<<",\n";
+  }
+  (*os_)<<"}\n";
+}
+
+}}
+
--- util.hpp
+++ util.hpp
@@ -1,9 +1,9 @@
 #ifndef JSON_UTIL_HPP_
 #define JSON_UTIL_HPP_
 
-#include <boost/variant/apply_visitor.hpp>
+#include "json.hpp"
+#include <boost/variant/static_visitor.hpp>
 #include <boost/spirit/include/karma.hpp>
-#include <boost/spirit/include/support_ostream_iterator.hpp>
 
 namespace yangacer {
 namespace json {
@@ -11,20 +11,9 @@ namespace karma = boost::spirit::karma;
 
 template <typename OutputIterator>
 struct escaped_string
-  : karma::grammar<OutputIterator, std::string()>
+: karma::grammar<OutputIterator, std::string()>
 {
-    escaped_string()
-      : escaped_string::base_type(esc_str)
-    {
-        esc_char.add('\b', "\\b")('\f', "\\f")('\n', "\\n")
-                    ('\r', "\\r")('\t', "\\t")('\\', "\\\\")
-                    ('\'', "\\\'")('\"', "\\\"")('/',"\\/")
-            ;
-        esc_str =   karma::lit("\"")
-                << *(esc_char | karma::byte_)
-                <<  karma::lit("\"")
-            ;
-    }
+    escaped_string();
     karma::rule<OutputIterator, std::string()> esc_str;
     karma::symbols<char, char const*> esc_char;
 };
@@ -38,48 +27,15 @@ struct print
   void operator()(T const& v) const
   { (*os_)<<v; }
   
-  void operator()(bool const b) const
-  {
-    b?(*os_)<<"true":(*os_)<<"false";
-  }
+  void operator()(bool const b) const;
 
-  void operator()(std::string const& s) const
-  { 
-    escaped_string<boost::spirit::ostream_iterator> str_gen;
-    boost::spirit::ostream_iterator sink(*os_);
-    karma::generate(sink, str_gen, s);
-    //(*os_)<<"\""<<s<<"\""; 
-  }
+  void operator()(std::string const& s) const;
 
-  template<typename T>
-  void operator()(std::deque<T> const &v) const
-  {
-    (*os_)<<"[";
-    for(size_t i=0;i<v.size();++i){
-      boost::apply_visitor(*this, v[i]);
-      if(i + 1 != v.size())
-        (*os_)<<",";
-    }
-    (*os_)<<"]";
-  }
+  void operator()(array_t const &v) const;
 
-  template<typename T>
-  void operator()(std::map<std::string, T> const &m) const
-  {
-    using namespace std;
-    (*os_)<<"{";
-    typename map<string,T>::const_iterator i = m.begin();
-    typename map<string,T>::const_iterator j = m.begin();
-    while(i != m.end()){
-      (*this)(i->first);
-      (*os_)<<" : ";
-      boost::apply_visitor(*this, i->second);
-      if(++i != m.end())
-        (*os_)<<",\n";
-    }
-    (*os_)<<"}\n";
-  }
+  void operator()(object_t const &m) const;
 
+  static escaped_string<boost::spirit::ostream_iterator> str_gen;
   std::ostream *os_;
 };
 
