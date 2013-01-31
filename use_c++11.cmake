818854641ed97fdb984c39920ad162988d6947b8
yangacer <yangacer@ARMVM185.(none)>
2013-01-31 15:28:36 +0800

    Upgrade cmake module. Fix empty array bug.

--- use_c++11.cmake
+++ use_c++11.cmake
@@ -5,7 +5,12 @@
 if(CMAKE_COMPILER_IS_GNUCXX)
   message ("GNUCXX(g++) is detected. Apply C++11 flags.")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+elseif(${CMAKE_CXX_COMPILER} MATCHES "Visual Studio 10")
+  message ("MSVC 10 is detected. C++11 is supported")
+elseif(${CMAKE_CXX_COMPILER} MATCHES "clang")
+  message ("Clang is detected. Apply C++11 flags.")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
 else()
-  message ("GNUCXX(g++) is the only compler supported currently")
+  message ("Unsupported compiler")
 endif()
 
